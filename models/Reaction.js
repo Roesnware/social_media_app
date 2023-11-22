96ef424a7f8cbf4aa891c86f30abfd5bab490fcb
Roesn <Bubwub04@icloud.com>
2023-11-22 15:30:03 -0500

    basis for reaction model

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,1 +1,9 @@
-hi;
+// import modules 
+const { Schema, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+// make schema for reactions
+const reactionSchema = new Schema();
+
+// export module
+module.exports = reactionSchema;
