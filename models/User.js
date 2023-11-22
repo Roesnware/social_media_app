5025eef7c43b7d8eb5c48127a289b529c26795b4
Roesn <Bubwub04@icloud.com>
2023-11-22 15:53:43 -0500

    user model schema

--- models/User.js
+++ models/User.js
@@ -2,7 +2,40 @@
 const { Schema, Types } = require('mongoose');
 
 // make schema for users
-const userSchema = new Schema();
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        friends:
+            [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: 'user',
+                },
+            ],
+        thoughts:
+            [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: 'thought',
+                },
+            ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
 
 // export module
 module.exports = userSchema;
