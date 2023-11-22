64ace4142ec3d9b197ee234f6aedaaea634897d8
Roesn <Bubwub04@icloud.com>
2023-11-22 16:04:12 -0500

    api routes index done

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,13 @@
-history;
+// import modules
+const router = require('express').Router();
+
+// import routes 
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+// routes for users and thoughts
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+// export module
+module.exports = router;
