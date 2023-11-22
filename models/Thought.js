a02b3bb7f6b1c36d44af6f99bda5d320816846df
Roesn <Bubwub04@icloud.com>
2023-11-22 15:29:22 -0500

    basis for thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,9 @@
-hi;
+// import modules 
+const { Schema, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+// make schema for thoughts
+const thoughtSchema = new Schema();
+
+// export module
+module.exports = thoughtSchema;
