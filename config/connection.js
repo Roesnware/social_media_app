5965afc6751978ed82bc1c06fc3e6a274aa0e5d7
Roesn <Bubwub04@icloud.com>
2023-11-22 15:25:48 -0500

    connection to mongoose

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,11 @@
-history;
+// import ocnnect and connection from mongoose
+const { connect, connection } = require('mongoose');
+
+// build connection string
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmedia';
+
+// connect using connection string 
+connect(connectionString);
+
+// export module 
+module.exports = connection;
