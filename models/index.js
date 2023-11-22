99b96830092aef9a502d176842de97297326073a
Roesn <Bubwub04@icloud.com>
2023-11-22 15:58:18 -0500

    fixed models index

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 // import models 
 const User = require('./User');
 const Thought = require('./Thought');
-const Reaction = require('./Reaction');
 
 // export models 
 module.exports = { User, Thought, Reaction };
