bd2e4c80f192c4aecf013da784a24e3dd5ce12d2
Roesn <Bubwub04@icloud.com>
2023-11-22 15:43:12 -0500

    thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,8 +1,31 @@
 // import modules 
-const { Schema, Types } = require('mongoose');
+const { Schema, model } = require('mongoose');
+const reaction = require('./Reaction');
 
 // make schema for thoughts
-const thoughtSchema = new Schema();
+const thoughtSchema = new Schema(
+    {
+        thoughtContent: {
+            type: String,
+            required: true,
+        },
+        createdOn: {
+            type: Date,
+            default: Date.now,
+        },
+        user: {
+            type: String,
+            required: true,
+        },
+        reactions: [reaction],
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
 
 // export module
 module.exports = thoughtSchema;
