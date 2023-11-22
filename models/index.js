929ffd1cb803c65104b1ec2bcb27ae5979628225
Roesn <Bubwub04@icloud.com>
2023-11-22 15:31:26 -0500

    basis for models index

--- models/index.js
+++ models/index.js
@@ -1,1 +1,7 @@
-hi;
+// import models 
+const User = require('./User');
+const Thought = require('./Thought');
+const Reaction = require('./Reaction');
+
+// export models 
+module.exports = { User, Thought, Reaction };
