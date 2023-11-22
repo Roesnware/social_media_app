085ab9a888d536dfc3ff634780f70f96c617e560
Roesn <Bubwub04@icloud.com>
2023-11-22 16:01:59 -0500

    router index done

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,12 @@
-history;
+// import modules 
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+// api routes
+router.use('/api', apiRoutes);
+
+// 404 page 
+router.use((req, res) => res.send('Wrong route!'));
+
+// export module
+module.exports = router;
