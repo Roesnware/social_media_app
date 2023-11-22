26347aeab316135c269f6cc65377c4ec7b419e79
Roesn <Bubwub04@icloud.com>
2023-11-22 15:28:50 -0500

    basis for user model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,9 @@
-hi;
+// import modules 
+const { Schema, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+// make schema for users
+const userSchema = new Schema();
+
+// export module
+module.exports = userSchema;
